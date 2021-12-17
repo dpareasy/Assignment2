5e69caed954640a3447ec55e1f037475d0d6d1b6
dpareasy <92155300+dpareasy@users.noreply.github.com>
2021-12-17 02:04:30 +0100

    Add files via upload
    final version of the assignment with comments

--- second_assignment/src/controller.cpp
+++ second_assignment/src/controller.cpp
@@ -10,6 +10,15 @@
 #define FRONT_DIM 160
 #define FRONT_LEFT_DIM 240
 #define LEFT_DIM 40
+//defining value for maximum angular 
+//and linear velocity
+#define MAX_LINEAR_VELOCITY 8
+#define MAX_ANGULAR_VELOCITY 8
+//defining value for minimum angular 
+//and linear velocity
+#define MIN_LINEAR_VELOCITY 0
+#define MIN_ANGULAR_VELOCITY 0
+
 
 //Here, as global variable are defined the array for visual ranges
 float right_dist[RIGHT_DIM];
@@ -21,10 +30,10 @@ float left_dist[LEFT_DIM];
 //Trheshold distance  from the wall setted to 1. 
 //This is the maximum distance from which the robot can approaches
 float dist_min = 1;
-float max_linear_velocity = 2.0;
-float max_angular_velocity = 2.5;
-
-int count = 10;
+//declaration of linear and angular velocity as
+//global variables
+float linear_velocity;
+float angular_velocity;
 
 //The publisher is defined as global variable because 
 //I have to initialize it in the Main function but 
@@ -32,13 +41,68 @@ int count = 10;
 ros::Publisher pub; 
 
 bool change_velocity(second_assignment::Velocity::Request &req, second_assignment::Velocity::Response &res){
-	res.x = req.req_velocity;
-	res.z = req.req_velocity;
-
-	max_linear_velocity = max_linear_velocity + res.x;
-	std::cout << "\nlinear velocity = "<< max_linear_velocity;
-	max_angular_velocity = max_angular_velocity + res.z;
-	std::cout << "\tangular velocity = "<< max_angular_velocity <<"\n"<<std::endl;
+	//getting the request from the client and give an answer
+	res.resp_change_velocity = req.req_change_velocity;
+	//answer handling:
+	//If answer value is equal to 1
+	//then increase linear and angular velocity after
+	//a control. Velocity can be increased only if it is
+	// lower than maximum velocity 
+	if(res.resp_change_velocity == 1.0)
+	{
+		//increasing linear velocity 
+		if(linear_velocity < MAX_LINEAR_VELOCITY)
+		{
+			linear_velocity = linear_velocity + 1;
+			std::cout << "linear velocity: "<< linear_velocity <<std::endl;
+		}
+		else
+		{
+			linear_velocity = MAX_LINEAR_VELOCITY;
+			std::cout << "Max linear velocity reached "<<std::endl;
+		}
+		//increase angular velocity 
+		if(angular_velocity < MAX_ANGULAR_VELOCITY)
+		{
+			angular_velocity = angular_velocity + 1;
+			std::cout << "linear velocity: "<< angular_velocity <<std::endl;
+		}
+		else
+		{
+			angular_velocity = MAX_ANGULAR_VELOCITY;
+			std::cout << "Max angular velocity reached "<<std::endl;
+		}
+	}
+	//If answer value is equal to 2
+	//then decrease linear and angular velocity after
+	//a control. Velocity can be decreased only if it is
+	// higher than minimum velocity 
+	if(res.resp_change_velocity == 2.0)
+	{
+		//decreasing linear velocity 
+		if(linear_velocity > MIN_LINEAR_VELOCITY)
+		{
+			linear_velocity = linear_velocity - 1;
+			std::cout << "linear velocity: "<< linear_velocity <<std::endl;
+		}
+		else
+		{
+			linear_velocity = MIN_LINEAR_VELOCITY;
+			std::cout << "Min linear velocity reached "<<std::endl;
+		}
+		//decrease angular velocity 
+		if(angular_velocity > MIN_ANGULAR_VELOCITY)
+		{
+			angular_velocity = angular_velocity - 1;
+			std::cout << "linear velocity: "<< angular_velocity <<std::endl;
+		}
+		else
+		{
+			angular_velocity = MIN_ANGULAR_VELOCITY;
+			std::cout << "Min angular velocity reached "<<std::endl;
+		}
+	}
+	
 	return true;
 }
 
@@ -99,20 +163,25 @@ void Callback(const sensor_msgs::LaserScan::ConstPtr& msg)//we have a pointer of
 		}
 	geometry_msgs::Twist my_vel;
 	second_assignment::Velocity vel_srv;
-
-	my_vel.linear.x = max_linear_velocity;
-
+	//declaration and initialization of linear and angular velocity 
+	my_vel.linear.x = linear_velocity;
+	my_vel.angular.z = 0;
+	//control of front distance from the wall
 	if (min_front < dist_min)
 	{
+		//if right distance is greater than left distance
+		//make the robot turn right
 		if(min_right > min_left)
 		{
-			my_vel.linear.x = 0.1*max_linear_velocity;
-			my_vel.angular.z = - max_angular_velocity;
+			my_vel.linear.x = 0.1*linear_velocity;
+			my_vel.angular.z = - angular_velocity;
 		}
+		//if left distance is greater than right distance
+		//make the robot turn left
 		if(min_right < min_left)
 		{
-			my_vel.linear.x = 0.1*max_linear_velocity;
-			my_vel.angular.z = max_angular_velocity;
+			my_vel.linear.x = 0.1*linear_velocity;
+			my_vel.angular.z = angular_velocity;
 		}
 	}
 	pub.publish(my_vel);
@@ -126,8 +195,11 @@ int main(int argc, char **argv)
 	ros::init(argc, argv, "robot_node");
 	ros::NodeHandle nh;
 	//define the subscriber to compute the distance from walls
-	ros::Subscriber sub = nh.subscribe("/base_scan", 1, Callback);//the topic in which the other nodes use to publish their position 
+	//the topic in which the other nodes use to publish their position 
+	ros::Subscriber sub = nh.subscribe("/base_scan", 1, Callback);
+	//define the publisher to send message for the velocity of the robot
 	pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
+	//define the server for changing velocity of the robot
 	ros::ServiceServer service = nh.advertiseService("/velocity", change_velocity);
 	ros::spin();
 
--- second_assignment/src/ui.cpp
+++ second_assignment/src/ui.cpp
@@ -4,51 +4,62 @@
 #include "std_srvs/Empty.h"
 using namespace std;
 
+//declaring clients for change_velocity request
+//and for reset position request
 ros::ServiceClient client1;
 ros::ServiceClient client2;
 
 int main(int argc, char **argv)
 {
-
+    //initialization of the velocity node
 	ros::init(argc, argv, "change_velocity_node");
 	ros::NodeHandle nh;
 
+    //defining clients for change_velocity request
+    //and for reset position request
 	client1 = nh.serviceClient<second_assignment::Velocity>("/velocity");
     client2 = nh.serviceClient<std_srvs::Empty>("/reset_positions");
-    
-
-   
+    //initializing variables: one for value to pass to the server
+    //and the other one for case handling 
     float val;
     char choice;
 
     second_assignment::Velocity vel_srv;
     std_srvs::Empty res_pos;
 
-  
+    //while loop for contantly asking the user to insert a command 
     while(1)
     {
         cout << "\nRobot controller, press:" <<endl;
-        cout << "d) if you want to increase velocity " <<endl;
-        cout << "a) if you want to decrease velocity" <<endl;
+        cout << "w) if you want to increase linear velocity " <<endl;
+        cout << "s) if you want to decrease linear velocity" <<endl;
+        cout << "a) if you want to increase angular velocity " <<endl;
+        cout << "d) if you want to decrease angular velocity " <<endl;
         cout << "r) if you want to reset the position " <<endl;
         cout << "q) if you want to quit the program " <<endl;
         cout << "command chosen: ";
+        //getting the user choice from the keyboard
         cin >> choice;
+        //choice handling:
+        //- choose A/a to increase velocity 
+        //- choose D/d to decrease velocity 
+        //- choose R/r to reset robot position
+        //- choose Q/q to quit the program
         switch(choice)
         {
-            case 'D':
-            case 'd':
+            case 'A':
+            case 'a':
                 cout << "\nVelocity has been increased"<<endl;
                 val = 1.0;
-                vel_srv.request.req_velocity = val;
+                vel_srv.request.req_change_velocity = val;
                 client1.waitForExistence();
                 client1.call(vel_srv);
                 break;
-            case 'A':
-            case 'a':
+            case 'D':
+            case 'd':
                 cout<<"\nVelocity has been decreased"<<endl;
-                val = -1.0;
-                vel_srv.request.req_velocity = val;
+                val = 2.0;
+                vel_srv.request.req_change_velocity = val;
                 client1.waitForExistence();
                 client1.call(vel_srv);
                 break;
@@ -65,7 +76,7 @@ int main(int argc, char **argv)
                 break;
             default:
                 cout<<"\nInvalid input, retry\n"<<endl;
-                break;        
+                break;
         }
        
     }
